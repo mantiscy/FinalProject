a414e3c2e5ba41bb0ea4ac7762d96524ea0c9dc6
mantiscy <mantiscy@gmail.com>
2014-03-23 09:26:37 +0000

    Added functionality to select items by clicking on image instead of checkbox

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,4 +21,56 @@ $(function(){
       'hoverEffect' : 'popout'
   });
 
+  $('.offer_submit').click(function(ev){
+      check_boxes1 = $('.cur_user_prods')
+      check_boxes2 = $('.other_user_prods')
+      var flag1 = false
+      var flag2 = false
+
+      //Check user's own items
+      for(var i=0; i<check_boxes1.length; i++)
+      {
+        if(check_boxes1[i].checked)
+          flag1 = true;
+      }
+
+      if(flag1 == false)
+      {
+        ev.preventDefault();
+        Apprise("Select at least one of your items to trade");
+      }
+
+      //Check other user's items
+      for(var i=0; i<check_boxes2.length; i++)
+      {
+        if(check_boxes2[i].checked)
+          flag2 = true;
+      }
+      
+      if(flag2 == false)
+      {
+        ev.preventDefault();
+        Apprise("Select at least one of the other user's items to trade");
+      }
+    });
+
+    console.log($( "input:checked" ));
+    $( "input:checked" ).closest('.offer_item_view').find('img').addClass('img_with_border');
+
+    // When user clicks on an image, check the checkbox and add border around image
+    $('.offer_item_view').click(function(ev){
+      ev.preventDefault();
+      if($(this).find('input:checkbox').is(':checked'))
+      {
+        $(this).find('input:checkbox').prop("checked", false);
+        $(this).find('img').removeClass('img_with_border');
+      }
+      else
+      {
+        $(this).find('input:checkbox').prop("checked", true);
+        $(this).find('img').addClass('img_with_border');
+      }
+      
+    });    
+
 });
